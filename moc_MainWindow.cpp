29f5fc1404224156e61451fa2d1ed6312b5c86dc
Loren Burkholder (via Travis CI) <55629213+LorenDB@users.noreply.github.com>
2020-10-31 23:16:47 +0000

    Deploy LorenDB/TotalReqall to github.com/LorenDB/TotalReqall.git:gh-pages

--- moc_MainWindow.cpp
+++ moc_MainWindow.cpp
@@ -1,19 +1,18 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MainWindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include <memory>
 #include "../MainWindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'MainWindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.15.1. It"
+#error "This file was generated using the moc from 5.12.8. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -22,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[6];
-    char stringdata0[67];
+    QByteArrayData data[7];
+    char stringdata0[84];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,11 +36,12 @@ QT_MOC_LITERAL(1, 11, 12), // "runMemorizer"
 QT_MOC_LITERAL(2, 24, 0), // ""
 QT_MOC_LITERAL(3, 25, 16), // "cleanUpMemorizer"
 QT_MOC_LITERAL(4, 42, 12), // "showAboutDlg"
-QT_MOC_LITERAL(5, 55, 11) // "resizeToFit"
+QT_MOC_LITERAL(5, 55, 11), // "resizeToFit"
+QT_MOC_LITERAL(6, 67, 16) // "setStatusMessage"
 
     },
     "MainWindow\0runMemorizer\0\0cleanUpMemorizer\0"
-    "showAboutDlg\0resizeToFit"
+    "showAboutDlg\0resizeToFit\0setStatusMessage"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,7 +51,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -59,16 +59,18 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   34,    2, 0x08 /* Private */,
-       3,    0,   37,    2, 0x08 /* Private */,
-       4,    0,   38,    2, 0x08 /* Private */,
-       5,    0,   39,    2, 0x08 /* Private */,
+       1,    1,   39,    2, 0x08 /* Private */,
+       3,    0,   42,    2, 0x08 /* Private */,
+       4,    0,   43,    2, 0x08 /* Private */,
+       5,    0,   44,    2, 0x08 /* Private */,
+       6,    1,   45,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QString,    2,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
 
        0        // eod
 };
@@ -83,13 +85,14 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 1: _t->cleanUpMemorizer(); break;
         case 2: _t->showAboutDlg(); break;
         case 3: _t->resizeToFit(); break;
+        case 4: _t->setStatusMessage((*reinterpret_cast< QString(*)>(_a[1]))); break;
         default: ;
         }
     }
 }
 
 QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
-    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    &QMainWindow::staticMetaObject,
     qt_meta_stringdata_MainWindow.data,
     qt_meta_data_MainWindow,
     qt_static_metacall,
@@ -117,13 +120,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 5;
     }
     return _id;
 }
---
+++ moc_MemorizeEdit.cpp
@@ -0,0 +1,152 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MemorizeEdit.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../MemorizeEdit.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MemorizeEdit.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.8. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MemorizeEdit_t {
+    QByteArrayData data[5];
+    char stringdata0[41];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MemorizeEdit_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MemorizeEdit_t qt_meta_stringdata_MemorizeEdit = {
+    {
+QT_MOC_LITERAL(0, 0, 12), // "MemorizeEdit"
+QT_MOC_LITERAL(1, 13, 4), // "done"
+QT_MOC_LITERAL(2, 18, 0), // ""
+QT_MOC_LITERAL(3, 19, 13), // "messageToUser"
+QT_MOC_LITERAL(4, 33, 7) // "message"
+
+    },
+    "MemorizeEdit\0done\0\0messageToUser\0"
+    "message"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MemorizeEdit[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x06 /* Public */,
+       3,    1,   25,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    4,
+
+       0        // eod
+};
+
+void MemorizeEdit::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MemorizeEdit *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->done(); break;
+        case 1: _t->messageToUser((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MemorizeEdit::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MemorizeEdit::done)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (MemorizeEdit::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MemorizeEdit::messageToUser)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MemorizeEdit::staticMetaObject = { {
+    &QTextEdit::staticMetaObject,
+    qt_meta_stringdata_MemorizeEdit.data,
+    qt_meta_data_MemorizeEdit,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MemorizeEdit::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MemorizeEdit::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MemorizeEdit.stringdata0))
+        return static_cast<void*>(this);
+    return QTextEdit::qt_metacast(_clname);
+}
+
+int MemorizeEdit::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QTextEdit::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MemorizeEdit::done()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void MemorizeEdit::messageToUser(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
