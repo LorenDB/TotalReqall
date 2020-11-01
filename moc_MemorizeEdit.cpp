93c3d834eee1296682df40acb73b251b504552e5
LorenDB (via Travis CI) <computersemiexpert@outlook.com>
2020-11-01 03:57:18 +0000

    Deploy LorenDB/TotalReqall to github.com/LorenDB/TotalReqall.git:gh-pages

--- moc_MemorizeEdit.cpp
+++ moc_MemorizeEdit.cpp
@@ -1,18 +1,19 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MemorizeEdit.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
+#include <memory>
 #include "../MemorizeEdit.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'MemorizeEdit.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.12.8. It"
+#error "This file was generated using the moc from 5.15.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -97,7 +98,7 @@ void MemorizeEdit::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
 }
 
 QT_INIT_METAOBJECT const QMetaObject MemorizeEdit::staticMetaObject = { {
-    &QTextEdit::staticMetaObject,
+    QMetaObject::SuperData::link<QTextEdit::staticMetaObject>(),
     qt_meta_stringdata_MemorizeEdit.data,
     qt_meta_data_MemorizeEdit,
     qt_static_metacall,
@@ -145,7 +146,7 @@ void MemorizeEdit::done()
 // SIGNAL 1
 void MemorizeEdit::messageToUser(QString _t1)
 {
-    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
     QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
 QT_WARNING_POP
--- moc_MemorizeWidget.cpp
+++ moc_MemorizeWidget.cpp
@@ -1,18 +1,19 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MemorizeWidget.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
+#include <memory>
 #include "../MemorizeWidget.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'MemorizeWidget.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.12.8. It"
+#error "This file was generated using the moc from 5.15.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -105,7 +106,7 @@ void MemorizeWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
 }
 
 QT_INIT_METAOBJECT const QMetaObject MemorizeWidget::staticMetaObject = { {
-    &QWidget::staticMetaObject,
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
     qt_meta_stringdata_MemorizeWidget.data,
     qt_meta_data_MemorizeWidget,
     qt_static_metacall,
@@ -153,7 +154,7 @@ void MemorizeWidget::done()
 // SIGNAL 1
 void MemorizeWidget::newStatus(QString _t1)
 {
-    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
     QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
 QT_WARNING_POP
